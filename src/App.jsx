e3a43857646f30ebd0c637b1335c758f0ee24139
Karol Sobolewski <karolsobolewski92@gmail.com>
2023-01-19 13:31:39 +0100

    Add eslint config

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count) {
+      console.log(`count`, count);
+    }
+  });
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          href="https://github.com/crucialfixpl/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src="/logo.svg" className="logo" alt="Scriptorium logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Work in progress</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
